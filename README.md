386c112d0af1185c4f0a292d9ed330e22d729578
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-07-19 00:28:31 +0200

    Add flags to build script and  update README

--- README.md
+++ README.md
@@ -4,4 +4,19 @@ Audio plugin built with the JUCE Framework
 
 ## Build & Run
 
-`./build.sh`
+```bash
+$ ./build.sh -h
+
+Usage: ./build.sh [-c|b|r]
+
+Options:
+  -c     Clean the build directory
+  -b     Build the program
+  -r     Run the program
+  -h     Print the help
+
+At least one flag must be specified
+
+Exit Status:
+Returns 0 unless an invalid option is given or the current directory cannot be read.
+```
--- build.sh
+++ build.sh
@@ -1,3 +1,50 @@
-cmake . -B buildexp
-cmake --build build
-build/BasicEq_artefacts/Debug/Standalone/BasicEq.exe
+#!/bin/bash
+
+set -e # Fail in case if any commands fail
+set -E # Print errtrace in case of failure
+set -T # Print functrace in case of failure
+
+YELLOW_COLOR='\033[1;33m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+
+print_help () {
+    echo "Usage: ./build.sh [-c|b|r]"
+    echo
+    echo "Options:"
+    echo "  -c     Clean the build directory"
+    echo "  -b     Build the program"
+    echo "  -r     Run the program"
+    echo "  -h     Print the help"
+    echo
+    echo "At least one flag must be specified"
+    echo
+    echo "Exit Status:"
+    echo "Returns 0 unless an invalid option is given or the current directory cannot be read."
+}
+
+while getopts cbrh opt; do
+    case $opt in
+        c)
+            echo -e "${YELLOW_COLOR}CLEANING...${NO_COLOR}"
+            rm -rv build/ && echo -e "${YELLOW_COLOR}CLEAN SUCCESSFUL...${NO_COLOR}"
+        ;;
+        b)
+            echo -e "${YELLOW_COLOR}BUILDING...${NO_COLOR}"
+            cmake . -B build
+            cmake --build build
+            echo -e "${YELLOW_COLOR}BUILD COMPLETED...${NO_COLOR}"
+        ;;
+        r)
+            echo -e "${YELLOW_COLOR}RUNNING...${NO_COLOR}"
+            build/BasicEq_artefacts/Debug/Standalone/BasicEq.exe
+        ;;
+        h)
+            print_help
+        ;;
+        *)  # unknown option
+            echo -e "${RED_COLOR}WRONG FLAG"
+        ;;
+    esac
+done
+
